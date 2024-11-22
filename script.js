59f9e1a92801c9d4dd3a0ca65f42a24cdf4e24d5
Anwar Rahim <anwargithub@gmail.com>
2024-11-22 20:42:05 +0500

    project completed in which I learn how i use e.target and .parentElement

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 import {products} from "./productList.js";
 
 const container = document.getElementById('container')
+const clearBtn = document.getElementById('clear-btn')
 
 let productsHTML = ` `
 
 for(let product of products){
     productsHTML += `
- <div class="product">
+ <div class="product on-offer">
     <h4>${product.name}</h4>
     <h3> $${product.price}</h3>
     <img src="${product.image}" alt="${product.image}">
@@ -18,5 +19,14 @@ for(let product of products){
 container.innerHTML = productsHTML
 
 container.addEventListener('click', (e)=>{
-   document.getElementById(e.target.id).parentElement.style.background = 'lightblue'
+   document.getElementById(e.target.id).parentElement.classList.add('purchased')
+    document.getElementById(e.target.id).parentElement.classList.remove('on-offer')
 })
+
+clearBtn.addEventListener('click', ()=>{
+    const productClearAll = document.getElementsByClassName('product')
+    for(let product of productClearAll){
+        product.classList.remove('purchased')
+    }
+})
+
